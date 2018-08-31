a3d0a4fc79be3e47bdc3222111fb7b5a5921d360
StojanovicMilos <StojanovicMilos@users.noreply.github.com>
2018-08-31 21:47:35 +0200

    Module3 (#2)
    * Lesson 2 - routes and controllers
    
    * Lesson 3 - Actions and Parameters
    
    * Lesson 4 Action results
    
    * Lesson 5 Action selectors
    
    * Lesson 6 Action filters

--- OdeToFood/App_Start/FilterConfig.cs
+++ OdeToFood/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace OdeToFood
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //filters.Add(new AuthorizeAttribute());
         }
     }
 }
---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System;
+using OdeToFood.Filters;
+
+namespace OdeToFood.Controllers
+{
+    //[Authorize]
+    [Log]
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        public ActionResult Search(string name = "french")
+        {
+            throw new Exception("Something terrble has happened!");
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+    }
+}
