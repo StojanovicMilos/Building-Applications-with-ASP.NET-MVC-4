aa6c809fb2fd2ea7f64617e8c8eca8ffff1a47aa
StojanovicMilos <s.milos@sbb.rs>
2018-08-31 21:45:55 +0200

    Lesson 6 Action filters

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,22 +1,20 @@
 ﻿using System.Web.Mvc;
+using System;
+using OdeToFood.Filters;
 
 namespace OdeToFood.Controllers
 {
+    //[Authorize]
+    [Log]
     public class CuisineController : Controller
     {
         // GET: Cuisine
-        [HttpPost]
         public ActionResult Search(string name = "french")
         {
+            throw new Exception("Something terrble has happened!");
             var message = Server.HtmlEncode(name);
 
             return Content(message);
-        }
-
-        [HttpGet]
-        public ActionResult Search()
-        {
-            return Content("Search!");
         }
     }
 }
