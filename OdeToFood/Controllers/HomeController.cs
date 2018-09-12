86e39f84ac29ba4be5ece332681ddca4122393e5
StojanovicMilos <s.milos@sbb.rs>
2018-09-12 21:08:37 +0200

    Lesson 3

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,20 +1,18 @@
 ﻿using OdeToFood.Models;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
         public ActionResult Index()
         {
-            var controller = RouteData.Values["controller"];
-            var action = RouteData.Values["action"];
-            var id = RouteData.Values["id"];
+            var model = _db.Restaurants.ToList();
 
-            var message = string.Format("{0}::{1} {2}", controller, action, id);
-            ViewBag.Message = message;
-
-            return View();
+            return View(model);
         }
 
         public ActionResult About()
@@ -31,6 +29,15 @@ namespace OdeToFood.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
