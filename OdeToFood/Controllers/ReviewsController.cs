a7e22d3a1a805ad9b8156c0cb346bc20f229f1c9
StojanovicMilos <s.milos@sbb.rs>
2018-09-29 21:30:48 +0200

    Lesson 4 - Creating a review

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -13,14 +13,32 @@ namespace OdeToFood.Controllers
         OdeToFoodDb _db = new OdeToFoodDb();
 
         // GET: Reviews
-        public ActionResult Index([Bind(Prefix ="id")]int restaurantId)
+        public ActionResult Index([Bind(Prefix = "id")]int restaurantId)
         {
             var restaurant = _db.Restaurants.Find(restaurantId);
-            if(restaurant!=null)
+            if (restaurant != null)
             {
                 return View(restaurant);
             }
             return HttpNotFound();
+        }
+
+        [HttpGet]
+        public ActionResult Create(int restaurantId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Reviews.Add(review);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+            return View(review);
         }
 
         protected override void Dispose(bool disposing)
