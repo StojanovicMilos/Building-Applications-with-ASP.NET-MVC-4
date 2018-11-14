1b9a4d511ccfa6eb8a31b2c9d1b939766efe1aa8
StojanovicMilos <StojanovicMilos@users.noreply.github.com>
2018-11-14 21:31:16 +0100

    Module6 (#5)
    * Lesson2
    
    * Lesson 3 - Listing reviews
    
    * Lesson 4 - Creating a review
    
    * Lesson 5 - Editing a review
    
    * Lesson 6 - Mass Assignment (Overposting)
    
    * Lesson 7 - Validation Annotations
    
    * Lesson 8 - custom validations

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -1,11 +1,31 @@
-﻿namespace OdeToFood.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Models
 {
-    public class RestaurantReview
+    public class RestaurantReview : IValidatableObject
     {
         public int Id { get; set; }
+
+        [Range(1,10)]
+        [Required]
         public int Rating { get; set; }
+
+        [Required]
+        [StringLength(1024)]
         public string Body { get; set; }
+
+        [Display(Name ="User name")]
+        [DisplayFormat(NullDisplayText ="anonymus")]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Rating < 2 && ReviewerName.ToLower().StartsWith("scott"))
+            {
+                yield return new ValidationResult("Sorry, Scott, you can't do this");
+            }
+        }
     }
 }
